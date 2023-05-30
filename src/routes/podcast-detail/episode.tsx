4f3d389bca882025333a2870a753b8b33baad4cb
Javier Maestre <7249124+jmaestree@users.noreply.github.com>
2023-05-30 17:18:27 +0200

    feat: :sparkles: implement Episode detail page

---
+++ src/routes/podcast-detail/episode.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Await, useParams, useRouteLoaderData } from 'react-router-dom';
+import { Podcast } from '../../api/podcasts';
+import Card from '../../components/card';
+
+const Episode: React.FC = () => {
+  const { episodeId } = useParams();
+  const { podcast } = useRouteLoaderData('detail') as { podcast: Podcast };
+
+  return (
+    <React.Suspense fallback={null}>
+      <Await resolve={podcast} errorElement={<p>Error loading podcasts!</p>}>
+        {(item: Podcast) => {
+          const episode = item?.episodes?.find((item) => item.id === episodeId);
+          const { title, description, audio } = episode ?? item.episodes?.[0] ?? {};
+
+          return (
+            <>
+              {!episode && (
+                <p className="text-orange-600">
+                  Unable to find the episode with ID: {episodeId}, showing the first one of the podcast
+                </p>
+              )}
+              <Card className="py-3 flex-col gap-y-4">
+                <h2 className="text-xl font-bold">{title}</h2>
+                <p className="italic" dangerouslySetInnerHTML={{ __html: description || '' }} />
+                <hr className="border-t border-gray-300" />
+                <audio controls className="w-full">
+                  <source src={audio?.url} type={audio?.type} />
+                  Your browser does not support the audio element.
+                </audio>
+              </Card>
+            </>
+          );
+        }}
+      </Await>
+    </React.Suspense>
+  );
+};
+
+export default Episode;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import PodcastList, { loader as loaderPodcastList } from './podcast-list/podcast-list';
 import PodcastDetail, { loader as loaderPodcastDetail } from './podcast-detail/podcast-detail';
 import Information from './podcast-detail/information';
+import Episode from './podcast-detail/episode';
 
 export const router = createBrowserRouter([
   {
@@ -21,7 +22,7 @@ export const router = createBrowserRouter([
       },
       {
         path: 'episode/:episodeId',
-        element: 'Episode detail'
+        element: <Episode />
       }
     ]
   }
