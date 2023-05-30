434b0d0afb2476a581ee6358043c97a1bf6e24b1
Javier Maestre <7249124+jmaestree@users.noreply.github.com>
2023-05-30 13:56:23 +0200

    refactor: :recycle: use Card in Podcast component

--- src/components/podcast.tsx
+++ src/components/podcast.tsx
@@ -1,4 +1,5 @@
 import { Podcast as PodcastType } from '../api/podcasts';
+import Card from './card';
 
 interface PodcastProps {
   podcast: PodcastType;
@@ -8,11 +9,11 @@ const Podcast: React.FC<PodcastProps> = ({ podcast }) => {
   const { title, artist, image } = podcast || {};
 
   return (
-    <div className="relative flex flex-col px-6 pb-3 bg-white shadow items-center mt-10 text-center">
+    <Card className="relative flex-col items-center mt-10 text-center">
       <img src={image} className="w-2/3 aspect-square rounded-full shadow-xl -mt-10" alt={title} />
       <h5 className="uppercase break-words line-clamp-2">{title}</h5>
       <p className="text-sm text-slate-500">Author: {artist}</p>
-    </div>
+    </Card>
   );
 };
 
