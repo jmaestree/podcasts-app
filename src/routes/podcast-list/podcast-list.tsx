01ac7e3bb04617b3de7f0bf92a5e7a1e9fd58236
Javier Maestre <7249124+jmaestree@users.noreply.github.com>
2023-05-30 18:26:18 +0200

    feat: :sparkles: implement search in podcast list

--- src/routes/podcast-list/podcast-list.tsx
+++ src/routes/podcast-list/podcast-list.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Await, Link, LoaderFunction, defer, useLoaderData } from 'react-router-dom';
+import { Await, LoaderFunction, defer, useLoaderData } from 'react-router-dom';
 import { Podcast as PodcastType, getAllPodcasts } from '../../api/podcasts';
-import Podcast from '../../components/podcast';
 import Navbar from '../../components/navbar';
+import List from './list';
 
 const PodcastList: React.FC = () => {
   const { podcasts } = useLoaderData() as { podcasts: PodcastType[] };
@@ -16,19 +16,11 @@ const PodcastList: React.FC = () => {
           </Await>
         </React.Suspense>
       </Navbar>
-      <div className="grid grid-cols-4 gap-x-4 gap-y-12">
-        <React.Suspense fallback={null}>
-          <Await resolve={podcasts} errorElement={<p>Error loading podcasts!</p>}>
-            {(podcasts: PodcastType[]) =>
-              podcasts.map((item) => (
-                <Link key={item.id} to={`podcast/${item.id}`} className="flex">
-                  <Podcast podcast={item} />
-                </Link>
-              ))
-            }
-          </Await>
-        </React.Suspense>
-      </div>
+      <React.Suspense fallback={null}>
+        <Await resolve={podcasts} errorElement={<p>Error loading podcasts!</p>}>
+          {(podcasts: PodcastType[]) => <List podcasts={podcasts} />}
+        </Await>
+      </React.Suspense>
     </>
   );
 };
